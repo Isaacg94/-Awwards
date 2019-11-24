0f2273a56e9b6cbeb0d9d905042d33de8ff6444d
Isaacg94 <7248zack@gmail.com>
2019-11-24 18:09:16 +0300

    install bootstrap and register it in the settings.py file

--- homage/urls.py
+++ homage/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^homageapp/',include('homageapp.urls'))
+    url(r'',include('homageapp.urls'))
 ]
