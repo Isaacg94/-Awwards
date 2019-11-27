7e56fb1f71f38b05755ada6e86151ee6da66a93b
Isaacg94 <7248zack@gmail.com>
2019-11-27 12:22:19 +0300

    add search view function to implement the seach feature

--- homageapp/views.py
+++ homageapp/views.py
@@ -6,3 +6,17 @@ from .models import Project,Profile
 def index(request):
     projects = Project.get_projects()
     return render(request,'index.html',{"projects":projects})
+
+
+def search_results(request):
+
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
